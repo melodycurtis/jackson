0bc3d76c5599e5f310633b5033a48e790d0857d1
melody curtis <m.curtis@kainos.com>
2018-11-21 10:34:42 +0000

    service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Book a driver theory test',
+  serviceName: 'Book your driver theory test',
 
   // Default port that prototype runs on
   port: '3000',
