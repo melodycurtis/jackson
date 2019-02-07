433e8cbacb31878a1ce0ceef5cbeea26dcce432f
Melody Curtis <melodycurtis123@googlemail.com>
2019-02-07 16:19:46 +0000

    new provider

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Learning provider details',
+  serviceName: 'Manage provider records',
 
   // Default port that prototype runs on
   port: '3000',
