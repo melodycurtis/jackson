c43b04222711d86805955a4effe9ef695926e5cc
Melody Curtis <melodycurtis123@googlemail.com>
2019-03-27 13:22:28 +0000

    admin-academy

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage provider records',
+  serviceName: 'Manage training provider records',
 
   // Default port that prototype runs on
   port: '3000',
