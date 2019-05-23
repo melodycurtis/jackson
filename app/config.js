15f20c26edfb4d61f15f3e10e6886baa1b3701e5
Melody Curtis <melodycurtis123@googlemail.com>
2019-05-23 11:06:56 +0100

    dash for show and tell

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage training provider records',
+  serviceName: 'Manage provider information',
 
   // Default port that prototype runs on
   port: '3000',
